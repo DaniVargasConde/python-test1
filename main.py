2a452ca9e31ed7ae1c0161be11273edaacb11839
DaniVargasConde <daniela.vargas.conde@ucsp.edu.pe>
2022-06-23 13:25:10 -0500

    fifth commit

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ def print_hi(name):
     print(f'Hi, {name}')
 def sum(a,b):
     return a+b
+def res(a,b):
+    return print(a-b)
 
 if __name__ == '__main__':
     x=1
     y=2
+
+    res(y,x)
     print(sum(x,y))
 
     print_hi('Soy Dani')
